6711d433c15bb4ab7ccbe27e871adbd5a4e5c5a8
Offek <Offek.anker@gmail.com>
2020-03-24 15:12:52 +0200

    Added IClockButton.cs and ButtonType.cs

---
+++ GShock.Common/Abstarct/IClockButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using GShock.Common.DTO;
+
+namespace GShock.Common.Abstarct
+{
+    public interface IClockButton
+    {
+        ButtonType Type { get; }
+
+        /// <summary>
+        /// Subscribe a delegate to a button action
+        /// </summary>
+        /// <param name="buttonAction">The action that decides if <param name="clickHandler"></param> is called</param>
+        /// <param name="clickHandler">the Action to run on a button click based on the delegate</param>
+        void Subscribe(Action<Action, int> buttonAction, EventHandler clickHandler);
+
+        /// <summary>
+        /// Removes an action from the internal action store
+        /// </summary>
+        /// <param name="buttonAction">The action to remove</param>
+        void Unsubscribe(Action<Action, int> buttonAction);
+    }
+}
