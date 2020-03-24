b89a825d3c3cd11695380ecba2acc327e06e13a1
Offek <Offek.anker@gmail.com>
2020-03-25 00:13:27 +0200

    added missing project files

---
+++ GShock.UI/Pages/Host.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GShock.UI.Pages
+{
+    public class Host : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
