a792da5ecf0ffe48f3717d68ba00132aad64571a
Offek <Offek.anker@gmail.com>
2020-03-25 00:10:41 +0200

    added basic bll

---
+++ GShock.BLL/GShockService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using GShock.Common.Abstract;
+
+namespace GShock.BLL
+{
+    public class GShockService
+    {
+        /// <summary>
+        /// Gets the mode currently running
+        /// </summary>
+        public IClockMode CurrentMode => _modes.Peek();
+
+        private readonly ICollection<IClockButton> _buttons;
+        private readonly Queue<IClockMode> _modes;
+
+        public GShockService(ICollection<IClockButton> buttons, IEnumerable<IClockMode> modes)
+        {
+            _buttons = buttons;
+            _modes = new Queue<IClockMode>(modes);
+        }
+    }
+}
